0137f4dbf9415b6f36ee39869d170cba0db0f1b0
Alexander Zemlyanukhin <a.zemlyanukhin@gmail.com>
2015-08-01 02:02:20 +0500

    commit #2

--- WeatherInGreenland/ViewController.h
+++ WeatherInGreenland/ViewController.h
@@ -7,9 +7,21 @@
 //
 
 #import <UIKit/UIKit.h>
+#import "YQL.h"
 
 @interface ViewController : UIViewController
 
+@property (strong, nonatomic) YQL *yql;
+
+@property (nonatomic, strong) IBOutlet UILabel *titleLabel;
+@property (nonatomic, strong) IBOutlet UILabel *conditionLabel;
+@property (nonatomic, strong) IBOutlet UILabel *temperatureLabel;
+@property (nonatomic, strong) IBOutlet UIView *sunriseSunsetView;
+@property (nonatomic, strong) IBOutlet UILabel *sunriseTimeLabel;
+@property (nonatomic, strong) IBOutlet UILabel *sunriseLabel;
+@property (nonatomic, strong) IBOutlet UILabel *sunsetTimeLabel;
+@property (nonatomic, strong) IBOutlet UILabel *sunsetLabel;
+@property (nonatomic, strong) IBOutlet UIWebView *forecastWebView;
 
 @end
 
--- WeatherInGreenland/ViewController.m
+++ WeatherInGreenland/ViewController.m
@@ -8,7 +8,16 @@
 
 #import "ViewController.h"
 
-@interface ViewController ()
+#define GREENLAND_QUERRY    @"select * from weather.forecast where woeid in (select woeid from geo.places(1) where text=\"greenland\")"
+#define FORECAST_PATH       @"query.results.channel.item.description"
+#define TITLE_PATH          @"query.results.channel.location.city"
+#define CONDITION_PATH      @"query.results.channel.item.condition.text"
+#define TEMPERATURE_PATH    @"query.results.channel.item.condition.temp"
+#define TEMP_UNIT_PATH      @"query.results.channel.units.temperature"
+#define SUNRISE_PATH        @"query.results.channel.astronomy.sunrise"
+#define SUNSET_PATH         @"query.results.channel.astronomy.sunset"
+
+@interface ViewController () <UIWebViewDelegate>
 
 @end
 
@@ -16,12 +25,70 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+
+    // load data from Yahoo!
+    self.yql = [[YQL alloc] init];
+    NSDictionary *results = [self.yql query:GREENLAND_QUERRY];
+    NSLog(@"%@", [results valueForKeyPath:@"query"]);
+    
+    // populate UI
+    self.titleLabel.text = [results valueForKeyPath:TITLE_PATH];
+    self.conditionLabel.text = [results valueForKeyPath:CONDITION_PATH];
+    self.temperatureLabel.text = [[results valueForKeyPath:TEMPERATURE_PATH] stringByAppendingFormat:@"°%@", [results valueForKeyPath:TEMP_UNIT_PATH]];
+    
+    self.sunriseTimeLabel.text = [results valueForKeyPath:SUNRISE_PATH];
+    self.sunsetTimeLabel.text = [results valueForKeyPath:SUNSET_PATH];
+    
+    NSString *itemDsc = [results valueForKeyPath:FORECAST_PATH];
+    [self.forecastWebView loadHTMLString:itemDsc baseURL:nil];
+    
+    // add animations
+    CGRect sunriseTimeFrame = self.sunriseTimeLabel.frame;
+    CGRect sunriseFrame = self.sunriseLabel.frame;
+    CGRect sunsetTimeFrame = self.sunsetTimeLabel.frame;
+    CGRect sunsetFrame = self.sunsetLabel.frame;
+    CGFloat sty = sunriseTimeFrame.origin.y;
+    CGFloat sy = sunriseFrame.origin.y;
+    sunriseTimeFrame.origin.y += self.sunriseSunsetView.frame.size.height;
+    sunriseFrame.origin.y += self.sunriseSunsetView.frame.size.height;
+    sunsetTimeFrame.origin.y -= self.sunriseSunsetView.frame.size.height;
+    sunsetFrame.origin.y -= self.sunriseSunsetView.frame.size.height;
+    
+    self.sunriseTimeLabel.frame = sunriseTimeFrame;
+    self.sunriseLabel.frame = sunriseFrame;
+    self.sunsetTimeLabel.frame = sunsetTimeFrame;
+    self.sunsetLabel.frame = sunsetFrame;
+    sunriseTimeFrame.origin.y = sty;
+    sunsetTimeFrame.origin.y = sty;
+    sunriseFrame.origin.y = sy;
+    sunsetFrame.origin.y = sy;
+    [UIView animateWithDuration:1.0
+                          delay:1.0
+                        options: UIViewAnimationOptionCurveEaseOut
+                     animations:^{
+                         self.sunriseTimeLabel.frame = sunriseTimeFrame;
+                         self.sunriseLabel.frame = sunriseFrame;
+                         
+                         self.sunsetTimeLabel.frame = sunsetTimeFrame;
+                         self.sunsetLabel.frame = sunsetFrame;
+                     }
+                     completion:^(BOOL finished){}];
 }
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+#pragma mark - UIWebViewDelegate
+
+- (BOOL)webView:(UIWebView *)webView shouldStartLoadWithRequest:(NSURLRequest *)request navigationType:(UIWebViewNavigationType)navigationType {
+    if (navigationType == UIWebViewNavigationTypeLinkClicked) {
+        // open links being clicked in Safari
+        [[UIApplication sharedApplication] openURL:request.URL];
+        return NO;
+    }
+    return YES;
 }
 
 @end
